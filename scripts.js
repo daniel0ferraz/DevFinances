9b90c7f312c5efc3cadf5696dcb4d74d07341efb
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-06-15 16:16:43 -0300

    Refatorando codigo aula03

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ const Modal = {
   }
 }
 
-const Transactions = [
+const transactions = [
   {
     id: 1,
     description: 'Luz',
@@ -40,15 +40,96 @@ const Transactions = [
 ]
 
 const Transaction = {
+  all: transactions,
   incomes() {
-    // Somar entradas
+    let income = 0;
+    // pegar todas as transaçoes
+    //para cada transação, se ela for maior que zero
+    Transaction.all.forEach(transaction => {
+      // se ela for > 0
+      if (transaction.amount > 0) {
+        // somar a uma variavel e retorna a variavel
+        income += transaction.amount;
+      }
+    })
+    return income;
   },
   expenses() {
-    // Somar as saidas
+    let expense = 0;
+    // pegar todas as transaçoes
+    //para cada transação, se ela for maior que zero
+    Transaction.all.forEach(transaction => {
+      // se ela for > 0
+      if (transaction.amount < 0) {
+        // somar a uma variavel e retorna a variavel
+        expense += transaction.amount;
+      }
+    })
+    return expense;
   },
   total() {
-    // Entradas - saidas
+    return Transaction.incomes() + Transaction.expenses();
   }
 }
 
-// Substituir os dados do html com os dados do JS
+// Substituir os dados do html com os dados do JavaScript
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+
+  },
+
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+            <img src="./assets/minus.svg" alt="Remover">
+        </td>
+      `
+    return html
+  },
+
+  updateBalance() {
+    document
+      .getElementById('incomeDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.incomes())
+    document
+      .getElementById('expenseDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document
+      .getElementById('totalDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.total())
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+
+    value = String(value).replace(/\D/g, "")
+    value = Number(value) / 100
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+
+    return signal + value
+  }
+}
+
+Transaction.all.forEach(function (transaction) {
+  DOM.addTransaction(transaction)
+})
+
+DOM.updateBalance()
